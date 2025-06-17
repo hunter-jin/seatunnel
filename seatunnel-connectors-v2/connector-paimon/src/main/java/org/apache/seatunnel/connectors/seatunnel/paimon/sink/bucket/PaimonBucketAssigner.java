55c9893aae120669b14691c80c819e04fbb83b0d
jinhui <jinhui@aoyayun.com>
2025-06-18 02:12:24 +0800

    fix: 1. paimon version upgrade to 1.1.1

--- seatunnel-connectors-v2/connector-paimon/src/main/java/org/apache/seatunnel/connectors/seatunnel/paimon/sink/bucket/PaimonBucketAssigner.java
+++ seatunnel-connectors-v2/connector-paimon/src/main/java/org/apache/seatunnel/connectors/seatunnel/paimon/sink/bucket/PaimonBucketAssigner.java
@@ -52,7 +52,7 @@ public class PaimonBucketAssigner {
         long dynamicBucketTargetRowNum =
                 ((FileStoreTable) table).coreOptions().dynamicBucketTargetRowNum();
         this.simpleHashBucketAssigner =
-                new SimpleHashBucketAssigner(numAssigners, assignId, dynamicBucketTargetRowNum);
+                new SimpleHashBucketAssigner(numAssigners, assignId, dynamicBucketTargetRowNum, -1);
         loadBucketIndex(fileStoreTable, numAssigners, assignId);
     }
 
